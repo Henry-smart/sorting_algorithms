104002d568e8f6ea7d4574b4809e3e2a64095790
henrysmart <henryeziechina2@gmail.com>
2023-02-26 06:20:45 +0100

    bubble_fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,33 @@
+/*
+ * File: 0-bubble_sort.c
+ * Auth: Sangwani P Zyambo
+ */
 #include "sort.h"
 
 /**
- *swap - swaps two integers.
- *@xp: Pointer to the first integer.
- *@yp: Pointer tothe second integer.
- *Return: Nothing.
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble sort algorith.
+ *
+ * @array: Array of integers to be sorted
+ * @size: The size of the @array
  */
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
 
-/**
- *bubble_sort - Sorts an array of integers in ascending order
- *@array: An array of integers to be sorted.
- *@size: Size of the array.
- *Return: Nothing.
- */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int temp;
 
-	if (array == NULL || size == 0)
-		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
+			/* if this pair is out of order */
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				/* swap them and remember something changed */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
